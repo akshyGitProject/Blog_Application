0259f8da1fd84df5cb1785014767d3f04f7f52f1
akshy-007 <akshaytakale71@gmail.com>
2025-04-06 09:38:08 +0530

    First Commit

---
+++ src/test/java/com/example/blog/BlogAppProject/BlogAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog.BlogAppProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
