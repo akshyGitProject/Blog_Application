c389b6cf8f6e4e3f791562b35590ed8a0225ae04
akshy-007 <akshaytakale71@gmail.com>
2025-04-06 11:07:08 +0530

    second commit

---
+++ src/main/java/com/example/blog/BlogAppProject/repositories/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.blog.BlogAppProject.repositories;
+
+import com.example.blog.BlogAppProject.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer>{
+    User findByEmail(String email);
+
+}
